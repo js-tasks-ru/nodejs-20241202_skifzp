00d7bab9c8d7290e0a79834a2f5021a1392fc905
Maksym <maksym.sholudko@xmcyber.com>
2024-12-28 15:53:31 -0500

    Fixed issues

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,5 +1,21 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { appendFileSync } from "node:fs";
 
+const ERROR_LOG_FILE_NAME = 'errors.log';
+
+@Catch(Error, HttpException)
 export class HttpErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const time = new Date().toISOString();
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const status = typeof exception.getStatus === 'function' ? exception.getStatus() : 500;
+    appendFileSync(ERROR_LOG_FILE_NAME, `[${time}] ${status} - ${exception.message}\n`);
+
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message,
+      timestamp: new Date().toISOString(),
+    });
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/pipes/parse-int.pipe.ts
+++ 04-request-lifecycle/01-nestjs-components/pipes/parse-int.pipe.ts
@@ -3,9 +3,9 @@ import { BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
 @Injectable()
 export class ParseIntPipe implements PipeTransform {
   transform(value: string): number {
-    const num = parseInt(value);
+    const num = parseInt(value, 10);
 
-    if (isNaN(num)) throw new BadRequestException (`"${value}" не является числом.`);
+    if (isNaN(num)) throw new BadRequestException (`"${value}" не является числом`);
 
     return num;
   }
