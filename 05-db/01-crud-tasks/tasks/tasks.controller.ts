4fb4bc326a0413d60040f8503ccc7a18bd0b7f0c
Maksym <maksym.sholudko@xmcyber.com>
2025-01-04 19:27:22 -0500

    Developed Get /tasks, Post /tasks

--- 05-db/01-crud-tasks/tasks/tasks.controller.ts
+++ 05-db/01-crud-tasks/tasks/tasks.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Patch, Delete, Body } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { Task } from "./entities/task.entity";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create() {}
+  create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksService.create(createTaskDto);
+  }
 
   @Get()
-  findAll() {}
+  findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
 
   @Get(":id")
   findOne() {}
--- 05-db/01-crud-tasks/tasks/tasks.service.ts
+++ 05-db/01-crud-tasks/tasks/tasks.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from "@nestjs/common";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { UpdateTaskDto } from "./dto/update-task.dto";
+import { Task } from "./entities/task.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {}
+  constructor(
+    @InjectRepository(Task)
+    private readonly taskRepository: Repository<Task>,
+  ) {}
 
-  async findAll() {}
+  create(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.save(createTaskDto);
+  }
 
-  async findOne(id: number) {}
+  findAll(): Promise<Task[]> {
+    return this.taskRepository.find();
+  }
 
-  async update(id: number, updateTaskDto: UpdateTaskDto) {}
+  findOne(id: number): Promise<Task> {
+    return this.taskRepository.findOne({ where: { id } });
+  }
 
-  async remove(id: number): Promise<void> {}
+  async update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    await this.taskRepository.update(id, updateTaskDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.taskRepository.delete(id);
+  }
 }
